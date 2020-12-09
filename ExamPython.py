32eb3a8c2a46c68d242d87fb9721fdd0af5c5fda
Sébastien REY <rey.babou35@free.fr>
2020-12-09 10:49:29 +0100

    Update ExamPython.py

--- ExamPython.py
+++ ExamPython.py
@@ -3,6 +3,7 @@ import random
 from colorama import init
 init()
 from colorama import Fore, Back, Style
+init(autoreset=True)
 
 mot1 = "vaches"
 mot2 = "pigeon"
@@ -15,16 +16,19 @@ mot8 = "coques"
 mot9 = "oignon"
 mot10 = "consul"
 mots = [mot1, mot2, mot3, mot4, mot5, mot6, mot7, mot8, mot9, mot10]
+
 a= random.choice(mots)
 print(a)
+
 lettre= input("quelle lettre voulez vous chercher? ")
+
 i=0
  
 for i in range(len(a)):
     if a[i] == lettre:
-         print(lettre, "est présent au caractère n°", i+1)
+         print(Back.RED + lettre, "est présent au caractère n°", i+1)
     else:
-        print(lettre,  "est absent au caractère n°", i+1)
+        print(Back.YELLOW + lettre,  "est absent au caractère n°", i+1)
     i=i+1  
 
 input()
